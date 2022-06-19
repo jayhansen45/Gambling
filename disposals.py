90abb102eda14de05daee875f640a9d082e2960d
Hansen <jhansen3@kpmg.com.au>
2022-06-19 09:38:27 +0930

    Lots of changes

--- disposals.py
+++ disposals.py
@@ -1,24 +1,16 @@
 """ TO DO
-Add in the time on ground filter
-    Maybe outlier if I can be fucked
-
-Add minimum games filter
-    Games played column maybe?
-    Or just do an if statement and set the average to 0 if < 7
-
-Pull out the odds from sportsbet
+Maybe outlier if I can be fucked
 
 Fix for when it isn't 13 games
 
-Add in the difference column formula
-
-Add in the max difference formula
+Try again with autosizing
 
 Add comments below
 
 """
 
 import openpyxl as xl
+import string
 import requests
 import statistics
 import numpy as np
@@ -29,39 +21,54 @@ from bs4 import BeautifulSoup
 warnings.filterwarnings("ignore")
 urls = ['https://afltables.com/afl/stats/teams/adelaide/2022_gbg.html', 'https://afltables.com/afl/stats/teams/brisbanel/2022_gbg.html', 'https://afltables.com/afl/stats/teams/carlton/2022_gbg.html', 'https://afltables.com/afl/stats/teams/collingwood/2022_gbg.html', 'https://afltables.com/afl/stats/teams/essendon/2022_gbg.html', 'https://afltables.com/afl/stats/teams/fremantle/2022_gbg.html', 'https://afltables.com/afl/stats/teams/geelong/2022_gbg.html', 'https://afltables.com/afl/stats/teams/goldcoast/2022_gbg.html', 'https://afltables.com/afl/stats/teams/gws/2022_gbg.html', 'https://afltables.com/afl/stats/teams/hawthorn/2022_gbg.html', 'https://afltables.com/afl/stats/teams/melbourne/2022_gbg.html', 'https://afltables.com/afl/stats/teams/kangaroos/2022_gbg.html', 'https://afltables.com/afl/stats/teams/padelaide/2022_gbg.html', 'https://afltables.com/afl/stats/teams/richmond/2022_gbg.html', 'https://afltables.com/afl/stats/teams/stkilda/2022_gbg.html', 'https://afltables.com/afl/stats/teams/bullldogs/2022_gbg.html', 'https://afltables.com/afl/stats/teams/westcoast/2022_gbg.html', 'https://afltables.com/afl/stats/teams/swans/2022_gbg.html']
 depth = 0
+games = 13
 
 workbook = xl.Workbook()
 worksheet = workbook.worksheets[0]
 
-worksheet.cell(1, 1).value = "Players"
+worksheet.cell(1, 1).value = "Team"
+worksheet.column_dimensions['B'].width = 15
+worksheet.cell(1, 2).value = "Players"
 worksheet.column_dimensions['A'].width = 15
-for i in range(2, 13+2):
-    worksheet.cell(1, i).value = "Round " + str(i-1)
+for i in range(3, games+3):
+    worksheet.cell(1, i).value = "Round " + str(i-2)
 worksheet.cell(1, i+1).value = "Average"
 worksheet.cell(1, i+2).value = "StDev"
-worksheet.cell(1, i+3).value = "20+ Percentage"
-worksheet.column_dimensions['P'].width = 15
-worksheet.cell(1, i+4).value = "20+ Req Odds"
-worksheet.column_dimensions['Q'].width = 15
-worksheet.cell(1, i+5).value = "20+ Odds"
-worksheet.cell(1, i+6).value = "20+ Difference"
-worksheet.column_dimensions['S'].width = 15
+worksheet.cell(1, i+3).value = "15+ Percentage"
+worksheet.cell(1, i+4).value = "15+ Req Odds"
+worksheet.cell(1, i+5).value = "15+ Odds"
+worksheet.cell(1, i+6).value = "15+ Difference"
 
-worksheet.cell(1, i+7).value = "25+ Percentage"
-worksheet.column_dimensions['T'].width = 15
-worksheet.cell(1, i+8).value = "25+ Req Odds"
-worksheet.column_dimensions['U'].width = 15
-worksheet.cell(1, i+9).value = "25+ Odds"
-worksheet.cell(1, i+10).value = "25+ Difference"
-worksheet.column_dimensions['W'].width = 15
+worksheet.cell(1, i+7).value = "20+ Percentage"
+worksheet.cell(1, i+8).value = "20+ Req Odds"
+worksheet.cell(1, i+9).value = "20+ Odds"
+worksheet.cell(1, i+10).value = "20+ Difference"
 
-worksheet.cell(1, i+11).value = "30+ Percentage"
-worksheet.column_dimensions['Y'].width = 15
-worksheet.cell(1, i+12).value = "30+ Req Odds"
-worksheet.column_dimensions['Z'].width = 15
-worksheet.cell(1, i+13).value = "30+ Odds"
-worksheet.cell(1, i+14).value = "30+ Difference"
-worksheet.column_dimensions['AB'].width = 15
+worksheet.cell(1, i+11).value = "25+ Percentage"
+worksheet.cell(1, i+12).value = "25+ Req Odds"
+worksheet.cell(1, i+13).value = "25+ Odds"
+worksheet.cell(1, i+14).value = "25+ Difference"
+
+worksheet.cell(1, i+15).value = "30+ Percentage"
+worksheet.cell(1, i+16).value = "30+ Req Odds"
+worksheet.cell(1, i+17).value = "30+ Odds"
+worksheet.cell(1, i+18).value = "30+ Difference"
+
+worksheet.cell(1, i+19).value = "35+ Percentage"
+worksheet.cell(1, i+20).value = "35+ Req Odds"
+worksheet.cell(1, i+21).value = "35+ Odds"
+worksheet.cell(1, i+22).value = "35+ Difference"
+worksheet.cell(1, i+23).value = "Max Difference"
+
+j=i
+i=0
+
+for i in range(1, j+23+1):
+    if i > 26:
+        worksheet.column_dimensions['A'+ string.ascii_uppercase[i-1-26]].width = 15       
+    else:
+        worksheet.column_dimensions[string.ascii_uppercase[i-1]].width = 15
+
 
 for site in urls:
     print(site)
@@ -70,7 +77,10 @@ for site in urls:
     soup = BeautifulSoup(webpage, "html.parser")
 
     table = soup.find_all(attrs={'class':'sortable'})
+    team = soup.find('h2').text
+    team = team.split('-')
 
+    
     messy_disposals = table[0].find_all('td')
     messy_tog = table[21].find_all('td')
 
@@ -98,14 +108,20 @@ for site in urls:
 
     games = i-2
     
-    players = []
+    gross_players = []
 
     i=0
 
     while (i < len(disposals)):
-        players.append(disposals.pop(i))
+        gross_players.append(disposals.pop(i))
         tog.pop(i)
         i=i+games+1
+
+    players = []
+
+    for i in range(0, len(gross_players)):
+        gross_players[i] = gross_players[i].split(', ')
+        players.append(gross_players[i][1] + " " + gross_players[i][0])
 
     i=games
 
@@ -134,32 +150,54 @@ for site in urls:
         sum = 0
         count = 0
         values = []
-        worksheet.cell(a+2+depth, 1).value = players[a]
+        worksheet.cell(a+2+depth, 1).value = team[0]
+        worksheet.cell(a+2+depth, 2).value = players[a]
         for t in range(0, games):
-            worksheet.cell(a+2+depth, t+2).value = int(disp[a][t])
-            if int(disp[a][t]) != 0:
+            worksheet.cell(a+2+depth, t+3).value = int(disp[a][t])
+            if int(time[a][t]) < 60:
+                worksheet.cell(a+2+depth, t+3).value = 0
+                
+            if worksheet.cell(a+2+depth, t+3).value != 0:
                 sum = sum + int(disp[a][t])
                 count = count + 1
                 values.append(int(disp[a][t]))
-        if count > 0:
+        if count > 6:
             avg = sum/count
         else:
             avg = 0
 
         if games < 13:
-            t = t+1
-        worksheet.cell(a+2+depth, t+3).value = avg
+            t = t + 1
+        worksheet.cell(a+2+depth, t+4).value = avg
         values_numpy = np.array(values)
-        worksheet.cell(a+2+depth, t+4).value = np.std(values_numpy)
-        worksheet.cell(a+2+depth, t+5).value = (1-norm.cdf(19, avg, np.std(values_numpy)))*100
-        worksheet.cell(a+2+depth, t+6).value = 1/(worksheet.cell(a+2+depth, t+5).value/100)
+        worksheet.cell(a+2+depth, t+5).value = np.std(values_numpy)
 
-        worksheet.cell(a+2+depth, t+9).value = (1-norm.cdf(24, avg, np.std(values_numpy)))*100
-        worksheet.cell(a+2+depth, t+10).value = 1/(worksheet.cell(a+2+depth, t+9).value/100)
+        worksheet.cell(a+2+depth, t+6).value = (1-norm.cdf(14, avg, np.std(values_numpy)))*100
+        worksheet.cell(a+2+depth, t+7).value = 1/(worksheet.cell(a+2+depth, t+6).value/100)
+        worksheet.cell(a+2+depth, t+9).value = "=T"+str((a+2+depth))+"-"+"S"+str(a+2+depth)
 
+        
+        worksheet.cell(a+2+depth, t+10).value = (1-norm.cdf(19, avg, np.std(values_numpy)))*100
+        worksheet.cell(a+2+depth, t+11).value = 1/(worksheet.cell(a+2+depth, t+10).value/100)
+        worksheet.cell(a+2+depth, t+13).value = "=X"+str((a+2+depth))+"-"+"W"+str(a+2+depth)
 
-        worksheet.cell(a+2+depth, t+13).value = (1-norm.cdf(29, avg, np.std(values_numpy)))*100
-        worksheet.cell(a+2+depth, t+14).value = 1/(worksheet.cell(a+2+depth, t+13).value/100)
+        worksheet.cell(a+2+depth, t+14).value = (1-norm.cdf(24, avg, np.std(values_numpy)))*100
+        worksheet.cell(a+2+depth, t+15).value = 1/(worksheet.cell(a+2+depth, t+14).value/100)
+        worksheet.cell(a+2+depth, t+17).value = "=AB"+str((a+2+depth))+"-"+"AA"+str(a+2+depth)
+
+        worksheet.cell(a+2+depth, t+18).value = (1-norm.cdf(29, avg, np.std(values_numpy)))*100
+        worksheet.cell(a+2+depth, t+19).value = 1/(worksheet.cell(a+2+depth, t+18).value/100)
+        worksheet.cell(a+2+depth, t+21).value = "=AF"+str((a+2+depth))+"-"+"AE"+str(a+2+depth)
+        
+        worksheet.cell(a+2+depth, t+22).value = (1-norm.cdf(34, avg, np.std(values_numpy)))*100
+        worksheet.cell(a+2+depth, t+23).value = 1/(worksheet.cell(a+2+depth, t+22).value/100)
+        worksheet.cell(a+2+depth, t+25).value = "=AJ"+str((a+2+depth))+"-"+"AI"+str(a+2+depth)
+
+        if avg > 0:
+            worksheet.cell(a+2+depth, t+26).value = ("=MAX(U" + str(a+2+depth) + ", Y" + str(a+2+depth) + ", AC" + str(a+2+depth) + ", AG" + str(a+2+depth) + ", AK" + str(a+2+depth) + ")")
+        else:
+            worksheet.cell(a+2+depth, t+26).value = 0
+
     depth = depth + len(players)
 
 
--- odds.py
+++ odds.py
@@ -1,16 +1,9 @@
 """
-Tidy EVERYTHING up
+NEXT STEPS:
+Pull out the games being played that day and filter to show just them
+    What ones were entered when user was prompted
 
-While loop that runs while input != "Run please"
-Stores the urls in an array
-
-While loop that loops through the array and gets data on all of the matches
-
-Add comments
-
-Fix if it is SUS
-
-Add in the 15+ column
+Add a "Cleanse" option to remove all odds and fill again
 
 """
 
@@ -29,153 +22,206 @@ from datetime import datetime, timedelta, date
 import shutil
 import os
 
-filename ="C:\\Users\\jhansen3\\OneDrive - KPMG\\Documents\\Python\\Gambling\\Disposals Tracking.xlsx"
-#filename = "C:\\Users\\jayha\\Documents\\Gambling\\Automated\\Disposals Tracking.xlsx"
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--incognito")
-os.chmod('C:\\Users\\jhansen3\\OneDrive - KPMG\\Documents\\Python\\Gambling\\chromedriver_win32\\chromedriver.exe', 0o755)
-
-driver = webdriver.Chrome(executable_path=r"C:\Users\jhansen3\OneDrive - KPMG\Documents\Python\Gambling\chromedriver_win32\chromedriver.exe")
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-driver.get('https://www.sportsbet.com.au/betting/australian-rules/afl/port-adelaide-v-sydney-6597348')
-
-element = driver.find_elements(By.XPATH, '//*[@data-automation-id="market-group-accordion-header-title"]')
-
-driver.implicitly_wait(10)
-
-for i in range(0, len(element)):
-    if element[i].text == "Top Markets":
-        element[i].click()
-
-driver.implicitly_wait(10)
-
-elements = driver.find_elements(By.XPATH, '//*[@data-automation-id="market-group-accordion-header"]')
-
-driver.implicitly_wait(10)
-
-for i in range(0, len(elements)):
-    if elements[i].text == "Disposal Markets":
-        elements[i].click()
-
-driver.implicitly_wait(10)
+urls = []
+url = ""
 
 
+while url != "Run Please":
+    url = input("Enter the URL of the game: ")
+    if url != "Run Please":
+        urls.append(url)
+
+j=0
+
+for j in range(0, len(urls)):
+    #Initiates all of the arrays
+    players_15 = []
+    odds_15 = []
+    players_20 = []
+    odds_20 = []
+    players_25 = []
+    odds_25 = []
+    players_30 = []
+    odds_30 = []
+    players_35 = []
+    odds_35 = []
+
+    #Chomedriver options for some reason
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--incognito")
+    os.chmod('C:\\Users\\jhansen3\\OneDrive - KPMG\\Documents\\Python\\Gambling\\chromedriver_win32\\chromedriver.exe', 0o755)
 
 
-disposals = driver.find_elements(By.XPATH, '//*[@data-automation-id="accordion-header"]')
+    #Switch the below comments when swapping laptop
+    filename ="C:\\Users\\jhansen3\\OneDrive - KPMG\\Documents\\Python\\Gambling\\Disposals Tracking.xlsx"
+    #filename = "C:\\Users\\jayha\\Documents\\Gambling\\Automated\\Disposals Tracking.xlsx"
 
-driver.implicitly_wait(10)
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 15 or More Disposals":
-        disposals[i].click()
-
-driver.implicitly_wait(10)
-
-messy_players_15 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
-messy_odds_15 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
-
-players_15 = []
-odds_15 = []
-players_20 = []
-odds_20 = []
-players_25 = []
-odds_25 = []
-players_30 = []
-odds_30 = []
-
-for i in range(0, len(messy_players_15)):
-    players_15.append(messy_players_15[i].text)
-    odds_15.append(messy_odds_15[i].text)
-
-driver.implicitly_wait(10)
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 15 or More Disposals":
-        disposals[i].click()
-
-driver.implicitly_wait(10)
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 20 or More Disposals":
-        disposals[i].click()
-
-driver.implicitly_wait(10)
+    driver = webdriver.Chrome(executable_path=r"C:\Users\jhansen3\OneDrive - KPMG\Documents\Python\Gambling\chromedriver_win32\chromedriver.exe")
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
-messy_players_20 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
-messy_odds_20 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
-
-for i in range(0, len(messy_players_20)):
-    players_20.append(messy_players_20[i].text)
-    odds_20.append(messy_odds_20[i].text)
-
-driver.implicitly_wait(10)
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 20 or More Disposals":
-        disposals[i].click()
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 25 or More Disposals":
-        disposals[i].click()
-
-messy_players_25 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
-messy_odds_25 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
-
-for i in range(0, len(messy_players_25)):
-    players_25.append(messy_players_25[i].text)
-    odds_25.append(messy_odds_25[i].text)
-
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 25 or More Disposals":
-        disposals[i].click()
+    #Driver to get the info from that specific URL
+    driver.get(urls[j])
 
 
-for i in range(0, len(disposals)):
-    if disposals[i].text == "To Get 30 or More Disposals":
-        disposals[i].click()
+    #Finds and clicks the Top Markets then the Disposal Markets
+    element = driver.find_elements(By.XPATH, '//*[@data-automation-id="market-group-accordion-header-title"]')
 
-messy_players_30 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
-messy_odds_30 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
+    for i in range(0, len(element)):
+        if element[i].text == "Top Markets":
+            element[i].click()
 
-for i in range(0, len(messy_players_30)):
-    players_30.append(messy_players_30[i].text)
-    odds_30.append(messy_odds_30[i].text)
+    elements = driver.find_elements(By.XPATH, '//*[@data-automation-id="market-group-accordion-header"]')
 
-driver.quit()
-
-games = 13
-
-workbook = xl.load_workbook(filename)
-sheet = workbook.worksheets[0]
-
-for m in range(1, 1048576):
-    if sheet.cell(m, 1).value is None:
-        break
-m=m-1
-
-for i in range(0, len(players_20)):
-    for a in range(1, m):
-        if players_20[i] == sheet.cell(a, 1).value:
-            sheet.cell(a, games+6).value = float(odds_20[i])
+    for i in range(0, len(elements)):
+        if elements[i].text == "Disposal Markets":
+            elements[i].click()
 
 
-for i in range(0, len(players_25)):
-    for a in range(1, m):
-        if players_25[i] == sheet.cell(a, 1).value:
-            sheet.cell(a, games+10).value = float(odds_25[i])
+    #Finds and clicks each of the disposal markets
+    disposals = driver.find_elements(By.XPATH, '//*[@data-automation-id="accordion-header"]')
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 15 or More Disposals":
+            disposals[i].click()
+
+    #Pulls the data into an array
+    messy_players_15 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
+    messy_odds_15 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
 
 
-for i in range(0, len(players_30)):
-    for a in range(1, m):
-        if players_30[i] == sheet.cell(a, 1).value:
-            sheet.cell(a, games+14).value = float(odds_30[i])
-            
+    #Appends the text to a new array
+    for i in range(0, len(messy_players_15)):
+        players_15.append(messy_players_15[i].text)
+        if messy_odds_15[i].text != "SUS":
+            odds_15.append(messy_odds_15[i].text)
+        else:
+            odds_15.append(0)
+
+    #Closes the accordion heading
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 15 or More Disposals":
+            disposals[i].click()
+
+
+    #As above
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 20 or More Disposals":
+            disposals[i].click()
+
+
+    messy_players_20 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
+    messy_odds_20 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
+
+    for i in range(0, len(messy_players_20)):
+        players_20.append(messy_players_20[i].text)
+        if messy_odds_20[i].text != "SUS":
+            odds_20.append(messy_odds_20[i].text)
+        else:
+            odds_20.append(0)
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 20 or More Disposals":
+            disposals[i].click()
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 25 or More Disposals":
+            disposals[i].click()
+
+    messy_players_25 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
+    messy_odds_25 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
+
+    for i in range(0, len(messy_players_25)):
+        players_25.append(messy_players_25[i].text)
+        if messy_odds_25[i].text != "SUS":
+            odds_25.append(messy_odds_25[i].text)
+        else:
+            odds_25.append(0)
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 25 or More Disposals":
+            disposals[i].click()
+
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 30 or More Disposals":
+            disposals[i].click()
+
+    messy_players_30 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
+    messy_odds_30 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
+
+    for i in range(0, len(messy_players_30)):
+        players_30.append(messy_players_30[i].text)
+        if messy_odds_30[i].text != "SUS":
+            odds_30.append(messy_odds_30[i].text)
+        else:
+            odds_30.append(0)
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 30 or More Disposals":
+            disposals[i].click()
+
+    for i in range(0, len(disposals)):
+        if disposals[i].text == "To Get 35 or More Disposals":
+            disposals[i].click()
+
+    messy_players_35 = driver.find_elements(By.XPATH, '//*[contains(@data-automation-id, "-column-grid-outcome-name")]')
+    messy_odds_35 = driver.find_elements(By.XPATH, '//*[@data-automation-id="price-text"]')
+
+    for i in range(0, len(messy_players_35)):
+        players_35.append(messy_players_35[i].text)
+        if messy_odds_35[i].text != "SUS":
+            odds_35.append(messy_odds_35[i].text)
+        else:
+            odds_35.append(0)
+
+    
+    driver.quit()
+
+    #Stores the data in the Disposals Tracking spreadsheet
+    workbook = xl.load_workbook(filename)
+    sheet = workbook.worksheets[0]
+
+    i = 1
+
+    games = 13
+    
+    #Finds the bottom cell
+    for m in range(1, 1048576):
+        if sheet.cell(m, 1).value is None:
+            break
+    m=m-1
+
+    #Loops through and stores the value in the correct cell
+    for i in range(0, len(players_15)):
+        for a in range(1, m):
+            if players_15[i] == sheet.cell(a, 2).value:
+                sheet.cell(a, games+7).value = float(odds_15[i])
+
+    for i in range(0, len(players_20)):
+        for a in range(1, m):
+            if players_20[i] == sheet.cell(a, 2).value:
+                sheet.cell(a, games+11).value = float(odds_20[i])
+
+
+    for i in range(0, len(players_25)):
+        for a in range(1, m):
+            if players_25[i] == sheet.cell(a, 2).value:
+                sheet.cell(a, games+15).value = float(odds_25[i])
+
+
+    for i in range(0, len(players_30)):
+        for a in range(1, m):
+            if players_30[i] == sheet.cell(a, 2).value:
+                sheet.cell(a, games+19).value = float(odds_30[i])
+
+    for i in range(0, len(players_35)):
+        for a in range(1, m):
+            if players_35[i] == sheet.cell(a, 2).value:
+                sheet.cell(a, games+23).value = float(odds_35[i])
+    j = j+1
 
 workbook.save("Disposals Tracking.xlsx")
 
