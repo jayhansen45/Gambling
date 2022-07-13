568add7352f503893a0ab6191b33fda076b16403
Hansen <jhansen3@kpmg.com.au>
2022-07-13 19:16:28 +0930

    Adding the soccer file

---
+++ Soccer/EPL - Copy.py
@@ -0,0 +1,21 @@
+copy the details from the baseball scraper
+
+going to need to work out how to only pull the games being played that day
+
+
+
+
+Info to get
+Home
+Draw
+Away
+Home no draw
+Away no draw
+Home double chacne
+away double chance
+home both score
+draw both score
+away both score
+both to score
+over
+under
